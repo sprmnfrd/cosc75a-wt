4b2f8a6f5c1a18c6f1f2934991262d4dd396aa08
TSGGS <sagioatscie@gmail.com>
2021-12-23 12:40:55 +0800

    added search box

--- manager/admin/edit-product.php
+++ manager/admin/edit-product.php
@@ -4,8 +4,29 @@
 
     changeTitle("../templates/header.php", "Adminitstrator - WT");
 ?>
-    <div id="content">
-        <h1>Update Existing Product</h1>
+    <div class="container mt-3">
+        <h1>Update Product</h1>
+        <form class="mt-4" action="./edit-product.php" method="get">
+            <div class="row">
+                <div class="col-10">
+                    <input class="form-control"type="text" name="update-search" list="product-list" placeholder="Search Product">
+                    <datalist id="product-list">
+                        <?php
+                            $sql = "SELECT p.product_code AS product_code, p.product_name AS product_name FROM products AS p WHERE product_end_timestamp IS NULL AND p.product_type != 4";
+                            $result = prepareSQL($conn, $sql);
+                            while($resultRow = mysqli_fetch_array($result)) {
+                                echo '
+                                    <option value="'.$resultRow["product_code"].'">'.$resultRow["product_name"].'</option>
+                                ';
+                            }
+                        ?>
+                    </datalist>
+                </div>
+                <div class="col">
+                    <button type="submit" class="btn btn-light" name="search-product" value="search">Search</button>
+                </div>
+            </div>
+        </form>
     </div>
 <?php
     require("../templates/footer.php");
