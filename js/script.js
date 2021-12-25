b0b9d499a08c95ec08cff52b80bfe9fa0b8ecbf9
TSGGS <sagioatscie@gmail.com>
2021-12-25 23:10:15 +0800

    added validation and error display

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
-function toggleUserMenu() {
-    document.addEventListener("mouseup", function(e) {
-        let element = document.getElementById("user_menu");
-        if(!element.contains(e.target)) {
-            element.focus();
-        }
-    });
+function displayUpdateData(product_code, product_name, product_type, product_image, price_amount, product_description) {
+    let pCode = document.getElementById("update-product-code");
+    let pName = document.getElementById("update-product-name");
+    let pType = document.getElementById("update-product-type");
+    let pImage = document.getElementById("update-product-image-display");
+    let prAmount = document.getElementById("update-product-price");
+    let pDescription = document.getElementById("update-product-desc");
 
-    document.getElementById("user_menu").classList.toggle("toggle");
+    pCode.value = product_code;
+    pCode.disabled = true;
+
+    pName.value = product_name;
+
+    pType.value = product_type;
+    pType.disabled = true;
+
+    pImage.src = "../../images/product_images/"+product_image;
+    document.getElementById("update-product-image-display-div").classList.remove("d-none");
+    prAmount.value = price_amount;
+    pDescription.value = product_description;
+}
+
+function toggleError(element_id, action) {
+    if(action === "show") {
+        document.getElementById(element_id).classList.remove("d-none");
+    } else {
+        document.getElementById(element_id).classList.add("d-none");
+    }
 }
 
--- manager/admin/new-product.php
+++ manager/admin/new-product.php
@@ -6,13 +6,18 @@
 ?>
     <div class="container mt-3">
         <h1>Add New Product</h1>
-        <form class="mt-4" action="./new-product.php" method="POST" enctype="multipart/form-data">
+        <form class="mt-4" action="./new-product.php" method="POST" enctype="multipart/form-data" autocomplete="off">
             <div class="row">
                 <div class="col-2">
                     <label class="col-form-label" for="new-product-code"><h4>Product Code</h4></label>
                 </div>
                 <div class="col">
-                    <input type="text" class="form-control" name="new-product-code" id="new-product-code" autofocus required>
+                    <div class="row">
+                        <input type="text" class="form-control" name="new-product-code" id="new-product-code" autofocus required>
+                    </div>
+                    <div class="row mb-2 d-none error" id="new-product-code-error">
+                        <span>Invalid Product Code</span>
+                    </div>
                 </div>
             </div>
             <div class="row">
@@ -20,7 +25,12 @@
                     <label class="col-form-label" for="new-product-name"><h4>Product Name</h4></label>
                 </div>
                 <div class="col">
-                    <input type="text" class="form-control" name="new-product-name" id="new-product-name" required>
+                    <div class="row">
+                        <input type="text" class="form-control" name="new-product-name" id="new-product-name" required>
+                    </div>
+                    <div class="row mb-2 d-none error" id="new-product-name-error">
+                        <span>Invalid Product Name</span>
+                    </div>
                 </div>
             </div>
             <div class="row">
@@ -28,16 +38,21 @@
                     <label class="col-form-label" for="new-product-type"><h4>Product Type</h4></label>
                 </div>
                 <div class="col">
-                    <select class="form-select" name="new-product-type" id="new-product-type" required>
-                        <option selected disabled>--SELECT PRODUCT TYPE--</option>
-                        <?php
-                            $sql = "SELECT * FROM types WHERE type_name != 'DISCOUNT' ORDER BY type_name ASC";
-                            $result = prepareSQL($conn, $sql);
-                            while($rowResult = mysqli_fetch_array($result)) {
-                                echo '<option value="'.$rowResult["type_id"].'">'.$rowResult["type_name"].'</option>';
-                            }
-                        ?>
-                    </select>
+                    <div class="row">
+                        <select class="form-select" name="new-product-type" id="new-product-type" required>
+                            <option selected disabled>--SELECT PRODUCT TYPE--</option>
+                            <?php
+                                $sql = "SELECT * FROM types WHERE type_name != 'DISCOUNT' ORDER BY type_name ASC";
+                                $result = prepareSQL($conn, $sql);
+                                while($rowResult = mysqli_fetch_array($result)) {
+                                    echo '<option value="'.$rowResult["type_id"].'">'.$rowResult["type_name"].'</option>';
+                                }
+                            ?>
+                        </select>
+                    </div>
+                    <div class="row mb-2 d-none error" id="new-product-type-error">
+                        <span>Invalid Product Type</span>
+                    </div>
                 </div>
             </div>
             <div class="row">
@@ -45,7 +60,12 @@
                     <label class="col-form-label" for="new-product-image"><h4>Product Image</h4></label>
                 </div>
                 <div class="col">
-                    <input type="file" class="form-control" name="new-product-image" id="new-product-image" required>
+                    <div class="row">
+                        <input type="file" class="form-control" name="new-product-image" id="new-product-image"><!-- required>-->
+                    </div>
+                    <div class="row mb-2 d-none error" id="new-product-image-error">
+                        <span>Invalid File</span>
+                    </div>
                 </div>
             </div>
             <div class="row">
@@ -53,7 +73,12 @@
                     <label class="col-form-label" for="new-product-price"><h4>Product Price</h4></label>
                 </div>
                 <div class="col">
-                    <input type="number" class="form-control" name="new-product-price" id="new-product-price" min="0" value="0" required>
+                    <div class="row">
+                        <input type="number" class="form-control" name="new-product-price" id="new-product-price" min="0" value="0" required>
+                    </div>
+                    <div class="row mb-2 d-none error" id="new-product-price-error">
+                        <span>Invalid Price</span>
+                    </div>
                 </div>
             </div>
             <div class="row">
@@ -61,7 +86,9 @@
                     <label class="col-form-label" for="new-product-desc"><h4>Product Description</h4></label>
                 </div>
                 <div class="col">
-                    <textarea class="form-control" name="new-product-desc" id="new-product-desc"></textarea>
+                    <div class="row">
+                        <textarea class="form-control" name="new-product-desc" id="new-product-desc"></textarea>
+                    </div>
                 </div>
             </div>
             <div class="row mt-5">
@@ -75,75 +102,112 @@
     require("../templates/footer.php");
 
     if(isset($_POST["new-product"])) {
+        $pCode = $_POST["new-product-code"];
+        $pName = $_POST["new-product-name"];
+        $pType = $_POST["new-product-type"] ?? "0";
+        $pPrice = $_POST["new-product-price"];
+        $pDesc = $_POST["new-product-desc"];
+
         $img = $_FILES["new-product-image"]["name"];
         $imgTmp = $_FILES["new-product-image"]["tmp_name"];
         $imgExt = pathinfo($img, PATHINFO_EXTENSION);
 
-        if(!isset($_POST["new-product-type"])) {
+        $emptyCode = isEmpty($pCode, "new-product-code");
+        $emptyName = isEmpty($pName, "new-product-name");
+        $emptyType = isEmpty($pType, "new-product-type");
+
+        echo $emptyCode." ".$emptyName." ".$emptyType;
+
+        $errorCode = false;
+        $errorImage = false;
+        $errorPrice = false;
+
+
+        $sql = "SELECT product_id, product_code FROM products WHERE product_code=?";
+        $result = prepareSQL($conn, $sql, "s", $pCode);
+        if(mysqli_num_rows($result) != 0) {
             echo '
                 <script>
-                    alert("Please select a product type.");
+                    toggleError("new-product-code-error", "show");
                 </script>
             ';
+
+            $errorCode = true;
         } else {
-            if(!isImage($imgExt)) {
-                echo '
-                    <script>
-                        alert("Please upload an image file.");
-                    </script>
-                ';
-            } else {
-                if($_POST["new-product-price"] == 0) {
-                echo '
-                    <script>
-                        alert("Please set a price.");
-                    </script>
-                ';
-                } else {
-                    $duplicate = false;
+            echo '
+                <script>
+                    toggleError("new-product-code-error", "hide");
+                </script>
+            ';
 
-                    $sql = "SELECT product_code, product_name FROM products";
-                    $result = prepareSQL($conn, $sql);
-                    while($resultRow = mysqli_fetch_array($result)) {
-                        if($resultRow["product_code"] === $_POST["new-product-code"]) {
-                            $duplicate = true;
+            $errorCode = false;
+        }
 
-                            echo '
-                                <script>
-                                    alert("Please enter a unique product code.");
-                                </script>
-                            ';
+        if(!isImage($imgExt)) {
+            echo '
+                <script>
+                    toggleError("new-product-image-error", "show");
+                </script>
+            ';
 
-                            break;
-                        }
-                    }
+            $errorImage = true;
+        } else {
+            echo '
+                <script>
+                    toggleError("new-product-image-error", "hide");
+                </script>
+            ';
 
-                    if(!$duplicate) {
-                        $rename = date('YmdHis').'_'.uniqid().'.'.$imgExt;
+            $errorCode = false;
+        }
+
+        if($pPrice >= 0) {
+            echo '
+                <script>
+                    toggleError("new-product-price-error", "hide");
+                </script>
+            ';
+
+            $errorPrice = false;
+        } else {
+            echo '
+                <script>
+                    toggleError("new-product-price-error", "show");
+                </script>
+            ';
+
+            $errorPrice = true;
+        }
+
+        if(!$emptyCode && !$emptyName && !$emptyType && !$errorCode && !$errorImage && !$errorPrice) {
+            $rename = date('YmdHis').'_'.uniqid().'.'.$imgExt;
         
-                        move_uploaded_file($imgTmp, "../../images/product_images/$rename");
-                
-                        $sql = "INSERT INTO products VALUES (NULL, ?, ?, ?, ?, ?, NULL, NULL)";
-                        prepareSQL($conn, $sql, "ssiss", $_POST["new-product-code"], $_POST["new-product-name"], $_POST["new-product-type"], $_POST["new-product-desc"], $rename);
+            move_uploaded_file($imgTmp, "../../images/product_images/$rename");
 
-                        $sql = "SELECT product_id FROM products WHERE product_code=?";
-                        $result = prepareSQL($conn, $sql, "s", $_POST["new-product-code"]);
-                        while($resultRow = mysqli_fetch_array($result)) {
-                            $productID = $resultRow["product_id"];
-
-                            $sql = "INSERT INTO prices VALUES (NULL, ?, ?, NULL, NULL)";
-                            prepareSQL($conn, $sql, "ii", $productID, $_POST["new-product-price"]);
-
-                            break;
-                        }
-                
-                        echo '
-                            <script>
-                                window.location.replace("dashboard.php");
-                            </script>
-                        ';
-                    }
-                }
+            while($products = mysqli_fetch_array($result)) {
+                $productID = $products["product_id"];
             }
+    
+            $sql = "INSERT INTO products VALUES (NULL, ?, ?, ?, ?, ?, NULL, NULL)";
+            prepareSQL($conn, $sql, "ssiss", $pCode, $pName, $pType, $pDesc, $rename);
+
+            $sql = "INSERT INTO prices VALUES (NULL, ?, ?, NULL, NULL)";
+            prepareSQL($conn, $sql, "ii", $productID, $pPrice);
+
+            echo '
+                <script>
+                    window.location.replace("dashboard.php");
+                </script>
+            ';
+            
+        } else {
+            echo '
+                <script>
+                    document.getElementById("new-product-code").value = '.json_encode($pCode).';
+                    document.getElementById("new-product-name").value = '.json_encode($pName).';
+                    document.getElementById("new-product-price").value = '.json_encode($pPrice).';
+                    document.getElementById("new-product-desc").value = '.json_encode($pDesc).';
+                </script>
+            ';
         }
     }
