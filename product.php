93bd621f2e36ec731694d575c2bb68b02b3a3923
TSGGS <sagioatscie@gmail.com>
2022-01-08 08:27:05 +0800

    added products.php

--- product.php
+++ product.php
@@ -2,8 +2,8 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <!-- <title>WALANG TATAK ONLINE STORE</title> -->
-    <link rel="stylesheet" href="style.css">
+    <title>WALANG TATAK ONLINE STORE</title>
+    <link rel="stylesheet" href="./css/productpage.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
   </head>
